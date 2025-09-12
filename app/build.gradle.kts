2f4e9610b88fc7ff285bb531859ebd43e47aaa76
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-09-13 00:47:53 +0530

    new patch version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.codewithdipesh.habitized"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1"
+        versionCode = 10
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
